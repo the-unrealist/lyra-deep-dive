a172851b094cf88e595c3fdc7b4362a27ace20a1
Matt <114629586+the-unrealist@users.noreply.github.com>
2023-04-13 20:49:19 -0700

    Delete LyraUserFacingExperienceDefinition.cpp
    Too early to have this file

--- LyraStarterGame/Source/LyraGame/GameModes/LyraUserFacingExperienceDefinition.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LyraUserFacingExperienceDefinition.h"
-
-// Matt: Omitted CreateHostingRequest implementation because it's for creating online sessions which we'll cover later.
